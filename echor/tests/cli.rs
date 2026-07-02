a5dde3d223ec280d9157a8c25d60f55b4263994c
Pradeep <kpradeep06.ds@gmail.com>
2026-07-02 15:26:46 +0000

    cli.rs updated

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -32,3 +32,32 @@ fn hello2() -> Result<()> {
 
     Ok(())
 }
+
+fn run(args: &[&str], expected_file: &str) -> Result<()>{
+    let expected = fs::read_to_string(expected_file)?;
+    let output = Command::cargo_bin("echor")?.args(args).output().expect("fail");
+
+    let stdout = String::from_utf8(output.stdout).expect("invalid utf-8");
+    assert_eq!(stdout, expected);
+    Ok(())
+}
+
+#[test]
+fn hello3() -> Result<()>{
+    run(&["Hello there"], "tests/expected/hello1.txt")
+}
+
+#[test]
+fn hello4() -> Result<()>{
+    run(&["Hello", "there"], "tests/expected/hello2.txt")
+}
+
+#[test]
+fn hello5() -> Result<()>{
+    run(&["Hello  there", "-n"], "tests/expected/hello1.n.txt")
+}
+
+#[test]
+fn hello6() -> Result<()>{
+    run(&["-n","Hello",  "there"], "tests/expected/hello2.n.txt")
+}
