e1e4caae52144c6357b02aef066dcb8a80a63c9f
Pradeep <kpradeep06.ds@gmail.com>
2026-08-22 16:34:37 +0000

    done

--- dl_with_rust/src/main.rs
+++
@@ -1,9 +0,0 @@
-use ndarray::array;
-use linfa::Dataset;
-
-fn main() {
-    let arr = array![[1,2,3],[4,5,6]];
-    let target = array![1, 0];
-    let data = Dataset::new(arr, target);
-    println!("{:?}", data);
-}
